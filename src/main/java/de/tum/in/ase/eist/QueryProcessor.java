a1db358c941b13e64b58121565b6ffce37983973
igor_ <ge78xav@mytum.de>
2022-07-15 11:07:22 +0200

    t

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -6,16 +6,21 @@ import org.springframework.stereotype.Service;
 public class QueryProcessor {
 
     public String process(String query) {
-		query = query.toLowerCase();
+        query = query.toLowerCase();
         if (query.contains("shakespeare")) {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
-           return "igor";
-        } else { // TODO extend the programm here
-            return "";
+            return "igor";
+        } else if (query.contains("10 plus 7")) {
+            return "17";
         }
+
+        else if (query.contains("largest: 981, 761, 64, 21")) {
+            return "981";
+        }
+        return "";
     }
 }
 
